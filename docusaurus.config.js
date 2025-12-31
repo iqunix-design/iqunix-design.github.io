0cd5c1e1f3e36481c8531c0b61017ea9ace1f18f
aiyuhang <yh.ai@qq.com>
2025-12-31 09:58:45 +0800

    feat: 修改首页

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,10 +48,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
         blog: {
           showReadingTime: true,
@@ -60,13 +57,11 @@ const config = {
             xslt: true,
           },
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
+          // routeBasePath: '/',
         },
         theme: {
           customCss: './src/css/custom.css',
